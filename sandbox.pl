1b5abeeab76859e8f75feb9314c1674d2c47d6f0
David Bradford <dbradford@omniti.com>
2014-07-11 10:30:50 -0400

    updated to decoder ring nomenclature and removed blog text

--- sandbox.pl
+++ sandbox.pl
@@ -1,69 +1,43 @@
 #!/usr/bin/perl
 
 use 5.14.0;
-# use warnings; # Usually yes, use warnings. Turning off for this snippet.
 use autodie;
 
-package Foo;
+package Decoder_Ring;
 
 sub new {
-    my ($class,$baz) = @_;
-    return bless { baz => $baz }, $class;
+    my ( $class, $data ) = @_;
+    return bless { data => $data }, $class;
 }
 
-sub process_baz {
+sub decode {
     my ($self) = @_;
-    return join('',(split('',$self->get_baz))[2,7,17,24,31,37,45,54,55,65]);
+    return join( '',
+        ( split( '', $self->get_data ) )
+          [ 2, 7, 17, 24, 31, 37, 45, 54, 55, 65, 72, 77 ] );
 }
 
-sub get_baz {
+sub get_data {
     my ($self) = @_;
-    return $self->{baz};
+    return $self->{data};
 }
 
 package main;
 
-my $foo = Foo->new("fsmy ydie uiehorlsbc ylmteklt aevlqloruasjcgu tqomqhqobaenm rooydz");
-print "first result:", $foo->process_baz, "\n";
+# Here's the normal object creation and method call to decode
+my $normal_object =
+  Decoder_Ring->new('?sSY y#ee Uiehor$cBc yL@rekLt AevLQYotUasjCGu!Z!a"');
+print "normal result: ", $normal_object->decode, "\n";
 
-my $pretender = bless {}, 'MyTemporaryClass';
-{
-    my $baz = 'l bvuvfaxe iulosxzuen jwzvcbbuey';
-    *{MyTemporaryClass::get_baz} = sub { return $baz };
-}
+# and here's the "fake" one:
+# First, bless into unique class name to avoid polluting your namespace
+my $pretender_object = bless {}, 'MyTemporaryClass';
+my $input_data = 'l2a*lP lu*j!BfeCxssov@osol aupu Zwv+us P'
+               . 'VSMvxekv vegA*crsY$L$2^ Sei!uVC$t!N?4.3';
 
-print "second result:", Foo::process_baz( $pretender ), "\n";
+# Next, push a method into the symbol table of the newly created class
+*{MyTemporaryClass::get_data} = sub { return $input_data };
 
-# "Class Abuse"
-#
-# This situation arose today and it got me curious...
-#
-# What if you needed to access an algorithm in a very simple class method
-#
-# ...BUT the functionality of the class is significant and would be a
-# pain-in-the-ass to set up and CPU-intensive to run, all to use one small
-# function...
-#
-# You could copy the algorithm, but what if it changes? Then you'll have to
-# change it twice. That's no good.
-#
-# I don't like either option.
-#
-# So what if I just call that function directly? In some cases, I certainly
-# could.
-#
-# In other cases, where the variable is in $self, I could just send a regular
-# hashref in with the value I needed.
-#
-# In this case, it calls an accessor method -- in my example here, get_baz()
-# -- to get the value of one of its own properties. This keeps with a some
-# OOP philosophies, but where does this leave me?
-#
-# Remember when you call an object's method (or a class method) that the
-# indirection syntax $self->method() or Class::Whatever->method() sends the
-# thing before the -> as the first parameter.
-#
-# So I can just call the function with a first parameter which is an object
-# that responds to the get_baz message
-#
+# Now our ad-hoc object will respond appropriately to the get_data call!
+print "pretender result: ", Decoder_Ring::decode($pretender_object), "\n";
 
