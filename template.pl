7e4b7f90167e8d315248e097e370d03d8bba8189
David Bradford <dbradford@omniti.com>
2014-07-28 17:59:39 -0400

    add word template wherever things should be replaced

--- template.pl
+++ template.pl
@@ -1,54 +1,54 @@
 #!/usr/bin/perl
 
-use 5.14.0;
-use autodie;
-use warnings FATAL => 'all';
+use Modern::Perl '2014';
+our $VERSION = 'v0.1.2';
 
-use IO::File;
+# use warnings FATAL => 'all'; #template?
 
-package Aaa01
-{
+package MyTemplateScript {
 
     use Carp;
     use Data::Dumper;
     use Hash::Util qw(lock_keys);
     our $VAR1;
 
-    my $persist_file = "$ENV{HOME}/.aaa01";
-    my $do_persist = 0;
+    my $persist_file = "$ENV{HOME}/.my_template_script";
+    my $do_persist   = 1;
 
-    my @keys = qw(
-      argv
-      bar
-      foo
-      bar
-    );
+    my @keys = qw( argv template_bar template_foo );
+
+    # ------------------ "MAIN" ------------------------
+    sub run {
+        my ( $self, @argv ) = @_;
+        $self->{argv} = \@argv;
+
+        $self->template_process1();
+        $self->template_process2();
+        $self->{template_foo} = 3;
+        $self->{template_bar} = { name => 'baz' };
+        $self->freeze();
+        return 0;    # return for entire script template
+    }
+
+    sub template_process1 {
+        my ($self) = @_;
+        print "template_howdy\n";
+    }
+
+    sub template_process2 {
+        my ($self) = @_;
+        print "template_hey\n";
+    }
 
     sub new {
         my ($class) = @_;
 
-        my $self = { };
+        my $self = {};
         bless $self, $class;
-        thaw(\$self);
+        thaw( \$self );
         lock_keys( %$self, @keys );
 
         return $self;
-    }
-
-    sub run {
-        my ($self,@argv) = @_;
-        $self->{argv} = \@argv;
-
-        $self->message();
-        $self->{foo} = 3;
-        $self->{bar} = {name=>'baz'};
-        $self->freeze();
-        return 0; # return for entire script
-    }
-
-    sub message {
-        my ($self) = @_;
-        print "howdy\n";
     }
 
     sub thaw {
@@ -56,13 +56,14 @@ package Aaa01
 
         my ($self) = @_;
 
-        my $ifh = IO::File->new($persist_file, '<');
-        return if (!defined $ifh);
+        my $ifh = IO::File->new( $persist_file, '<' );
+        return if ( !defined $ifh );
 
         my $contents = do { local $/; <$ifh> };
         $ifh->close;
 
         ${$self} = eval $contents;
+        warn "Thawed! (template)\n", Dumper($self);
         if ( !defined $self ) {
             croak "failed eval of dump";
         }
@@ -73,8 +74,8 @@ package Aaa01
 
         my ($self) = @_;
 
-        my $ofh = IO::File->new($persist_file, '>');
-        croak "Failed to open output file: $!" if (!defined $ofh);
+        my $ofh = IO::File->new( $persist_file, '>' );
+        croak "Failed to open output file: $!" if ( !defined $ofh );
 
         print $ofh Dumper($self);
         $ofh->close;
@@ -84,6 +85,6 @@ package Aaa01
 
 package main;
 
-my $app = Aaa01->new();
+my $app = MyTemplateScript->new();    # TEMPLATE change
 exit $app->run(@ARGV);
 
