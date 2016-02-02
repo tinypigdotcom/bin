bbbe1c58e743fc9065d83c309b382720ab249a7e
David Bradford <dbradford@omniti.com>
2016-02-02 07:57:59 -0500

    add show slide

--- slides.pl
+++ slides.pl
@@ -5,13 +5,58 @@ use strict;
 use warnings FATAL => 'all';
 
 use Data::Dumper;
+use DMB::Notes;
 use DMB::Tools ':all';
+use Term::ReadLine;
 
 our $VERSION = '0.0.1';
+
+my $term;
+my %subs = (
+    'h' => { does => 'Get Help',
+            code => \&cmd_help },
+    'q' => { does => 'Quit',
+            code => sub { exit } },
+    's' => {
+        does => 'Get Slide',
+        code => sub {
+            my $file = "$ENV{HOME}/monstercards/dnd.note";
+            my $dmb_note = DMB::Notes->new(
+                file      => $file,
+                title     => 1,
+            );
+            my @output_files = $dmb_note->search_note('claw');
+            print @output_files;
+        } },
+);
+
+sub cmd_help {
+    for ( sort { $a cmp $b } keys %subs ) {
+        printf "%-3s %-34s\n", $_, $subs{$_}->{does};
+    }
+}
+
+sub add_history {
+    warn "$_[0]";
+    $term->addhistory($_[0]);
+}
 
 sub function1 {
     my ($lines) = @_;
     print "# of lines: $lines\n";
+    $term = $term = Term::ReadLine->new('slides');
+    $term->MinLine(undef); # disable autohistory
+    my $prompt = "slides> ";
+    while ( defined( my $line = $term->readline($prompt) ) ) {
+        chomp $line;
+        $line =~ m{(\S+)\s?};
+        my $first_word = $1 || '';
+        if ($subs{$first_word}) {
+            my $coderef = $subs{$first_word}->{code};
+            $coderef->($line);
+        }
+        add_history($line) if $line =~ /\S/;
+    }
     return;
 }
 
@@ -24,5 +69,7 @@ sub main {
 my $rc = ( main(@ARGV) || 0 );
 
 exit $rc;
+
+
 
 
